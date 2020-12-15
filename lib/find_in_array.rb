487d9a92003da17a73985307d101c904e517ea9d
zed <zed@kolk.me>
2020-12-15 23:00:19 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,8 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  count = 0
+  while count < array.length do
+    array[count] == value_to_find
+    count += 1
+  end
+  return count
 end
