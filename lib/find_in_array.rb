acf0f0af9bd159c5eed15eb16cc43dce9bfeec36
zed <zed@kolk.me>
2020-12-15 22:51:26 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,3 @@
 def find_element_index(array, value_to_find)
-  index_position =
-  while array.include?(value_to_find) true do
-    index_position = array.index(value_to_find)
-  end
-  index_position
+  array.index(value_to_find)
 end
