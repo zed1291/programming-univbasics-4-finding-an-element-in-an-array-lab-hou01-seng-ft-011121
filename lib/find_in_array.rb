feb086c6dc7f4aca11d176264913c5468b00406d
zed <zed@kolk.me>
2020-12-15 22:48:42 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,7 @@
 def find_element_index(array, value_to_find)
-  
+  index_position =
+  while array.include? (value_to_find) true do
+    index_position = array.index(value_to_find)
+  end
+  index_position
 end
